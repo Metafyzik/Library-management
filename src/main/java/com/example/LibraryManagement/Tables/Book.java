8fb9c3321c2aa443d7dd3b10f315f33363a9a7cd
Igor Kuruc <krc13@email.cz>
2025-03-18 12:22:20 +0100

    adding tables

---
+++ src/main/java/com/example/LibraryManagement/Tables/Book.java
@@ -0,0 +1,25 @@
+package com.example.LibraryManagement.Tables;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    private boolean isAvailable = true;
+}
---
+++ src/main/java/com/example/LibraryManagement/Tables/Loan.java
@@ -0,0 +1,30 @@
+package com.example.LibraryManagement.Tables;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "loans")
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private boolean returned;
+}
