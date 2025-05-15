897170843e31b3a236e915555c0465fd4bb79ede
Igor Kuruc <krc13@email.cz>
2025-05-15 21:47:18 +0200

    make DTO classes records

--- src/main/java/com/example/LibraryManagement/DTO/AuthRequest.java
+++ src/main/java/com/example/LibraryManagement/DTO/AuthRequest.java
@@ -1,13 +1,4 @@
 package com.example.LibraryManagement.DTO;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class AuthRequest {
-    private String username;
-    private String password;
+public record AuthRequest(String username, String password) {
 }
--- src/main/java/com/example/LibraryManagement/DTO/AuthResponse.java
+++ src/main/java/com/example/LibraryManagement/DTO/AuthResponse.java
@@ -1,12 +1,4 @@
 package com.example.LibraryManagement.DTO;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class AuthResponse {
-    private String token;
-    public AuthResponse(String token) { this.token = token; }
+public record AuthResponse(String token) {
 }
